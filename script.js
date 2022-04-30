fcaa99c77cc269c29de1458f43af4cb92625e172
Lena <elena.pokhvalennaya@gmail.com>
2022-04-30 10:38:12 +0300

    add files

---
+++ script.js
@@ -0,0 +1,31 @@
+const albums = document.getElementById("albums");
+
+fetch("https://jsonplaceholder.typicode.com/albums")
+  .then((response) => response.json())
+  .then((data) => {
+    for (object in data) {
+      const albumTitle = data[object].title;
+      const li = document.createElement("li");
+
+      li.innerHTML = albumTitle;
+      li.className = "album_title";
+      albums.append(li);
+
+      let button = document.createElement("button");
+
+      button.innerHTML = "delete";
+      button.className = "delete";
+      li.append(button);
+
+      albums.onclick = (event) => {
+        const target = event.target;
+        const isButton = target.className === "delete";
+
+        if (isButton) {
+          const liBlock = target.closest(".album_title");
+          liBlock.remove();
+        }
+      };
+    }
+  })
+  .catch((error) => console.log(error));
